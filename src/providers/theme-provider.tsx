215629bbc011b2c77f8257c0ca6d8d9fda1db5a3
DarshanAjudiya7 <darshanajudiya07@gmail.com>
2026-09-05 22:46:07 +0530

    resolve themeprovider

--- src/providers/theme-provider.tsx
+++ src/providers/theme-provider.tsx
@@ -7,5 +7,12 @@ export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return (
+    <NextThemesProvider
+      {...props}
+      scriptProps={{ async: true, ...props.scriptProps }}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
