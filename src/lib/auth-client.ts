1322546159a39448af5667838f2431ee4e8f4e25
Urvil Patel <uvpatel7271@gmail.com>
2026-09-05 10:31:40 +0530

    final rbac

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,17 @@
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins"
+import { organizationClient } from "better-auth/client/plugins"
+
+import {
+  ac,
+  employeeRole,
+  hrManagerRole,
+  payrollUserRole,
+  payrollManagerRole,
+  adminRole,
+} from "@/lib/auth/permission"
+
+
 
 export const authClient = createAuthClient({
   baseURL:
@@ -7,8 +19,18 @@ export const authClient = createAuthClient({
      process.env.BETTER_AUTH_URL  ||
     (typeof window !== "undefined" ? window.location.origin : "http://localhost:3000"),
     plugins: [
-        adminClient()  
-    ]
+    organizationClient({
+      ac,
+
+      roles: {
+        employee: employeeRole,
+        hr_manager: hrManagerRole,
+        payroll_user: payrollUserRole,
+        payroll_manager: payrollManagerRole,
+        admin: adminRole,
+      },
+    }),
+  ],
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
